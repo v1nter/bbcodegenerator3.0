7c87d69ede963ca3247e171589325d06ca3d4545
v1nter <sebastiankleinhaltern@gmail.com>
2023-12-19 17:21:01 +0100

    Spiele Desktop

--- src/app/(sites)/Spiele/page.tsx
+++ src/app/(sites)/Spiele/page.tsx
@@ -1,8 +1,9 @@
 import { checkEnvironment } from '@/app/lib/checkEnvironment';
 import triggerRevalidate from '@/app/lib/triggerRevalidate';
-import { Game } from '@prisma/client';
+import { Game, Platform, GamePlatform } from '@prisma/client';
 import { Fragment } from 'react';
 import css from './page.module.css';
+import Link from 'next/link';
 import { RxUpdate } from 'react-icons/rx';
 
 export const dynamic = 'force-dynamic';
@@ -12,6 +13,10 @@ export default async function Games() {
 	triggerRevalidate('/(sites)/Spiele');
 
 	const response = await fetch(`${host}/api/Games/GetGames`);
+	// const test = await fetch(`${host}/api/Games/GetGames`);
+
+	// console.log(await test.json());
+
 	const games = (await response.json()) as Game[];
 
 	return (
@@ -20,22 +25,27 @@ export default async function Games() {
 			<div className={css.GamesWrapper}>
 				{games.map((game) => (
 					<div className={css.GameInfos} key={game.game_id}>
-						<div>
+						<div className={css.GameInfoTableContainer}>
 							<table className={css.InfoTable}>
 								<tbody>
 									<tr>
 										<td className={css.InfoTableValue}>{game.game_name}</td>
+									</tr>
+									<tr className={css.InfoTableValueExtended}>
 										<td className={css.InfoTableValue}>
-											<button className={css.UpdateBtn}>
-												<RxUpdate className={css.BtnIcon} />
-											</button>
+											{game.game_release_date}
 										</td>
 									</tr>
 								</tbody>
 							</table>
 						</div>
 						<div className={css.GameArtworkContainer}>
-							<img src={game.game_keyart} className={css.GameArtwork} />
+							<button className={css.UpdateBtn}>
+								<RxUpdate className={css.ButtonIcon} /> Update
+							</button>
+							<Link className={css.GameArtWorkLink} href="/Spiele">
+								<img src={game.game_keyart} className={css.GameArtwork} />
+							</Link>
 						</div>
 					</div>
 				))}
--- src/app/api/Games/GetGames/route.ts
+++ src/app/api/Games/GetGames/route.ts
@@ -4,6 +4,13 @@ export const dynamic = 'force-dynamic';
 
 export async function GET(request: Request) {
 	const games = await prisma.game.findMany({
+		include: {
+			GamePlatform: {
+				include: {
+					platform: true,
+				},
+			},
+		},
 		orderBy: [{ game_name: 'asc' }],
 	});
 
