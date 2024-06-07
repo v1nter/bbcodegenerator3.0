bced2586be5313b21c1f6176c8285a2624875549
v1nter <sebastiankleinhaltern@gmail.com>
2024-06-07 20:07:50 +0200

    Change Access Token

--- src/app/api/IGDB/GetGame/route.ts
+++ src/app/api/IGDB/GetGame/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
 	const client_id = process.env.IGDB_CLIENT_ID as string;
-	const auth = 'Bearer 06yz5sr23il4qcck2iral0qr2lhak8'; // Der Einfachhalt halber erstmal hardcoded. Muss eigentlich aus der DB ausgelesen und regelmäßig refreshed werden
+	const auth = 'Bearer aoet6vauviklip6dml26svnmla3pk3'; // Der Einfachhalt halber erstmal hardcoded. Muss eigentlich aus der DB ausgelesen und regelmäßig refreshed werden
 
 	const game = request.nextUrl.searchParams.get('game') as string;
 
